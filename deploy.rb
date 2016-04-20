cdfda1fb2baaede77964a3e7898b71719fe5aa6b
Dan Gillespie <dan@redspread.com>
2016-04-20 16:43:10 -0700

    added deployment helpers

---
+++ deploy.rb
@@ -0,0 +1,46 @@
+# deploy attempts to recreate the given state on the connected cluster. It does this by first trying to create the object.A
+# If 'replace' is true, then it will try to replace the object if it already exits in the cluster.
+# false is returned if the operation is successful
+def deploy(data, replace=false, force=false)                                   ``
+    err = runWithData("kubectl create -f -", data)
+    if !$?.success?
+        if existsErr(err) && replace
+            return replace(data, force)
+        end
+        return "could not create object: #{err}"
+    end
+    return false
+end
+
+# replace attempts to modify an existing object within a cluster
+# if 
+# false is returned if the operation is successful
+def replace(data, force=false)
+    args = ""
+    if force
+        args += "--force" 
+    end
+
+    cmd = "kubectl replace -f - #{args}"
+    err = runWithData(cmd, data)
+    if err
+        return "could not replace object: #{err}"
+    end
+    return false
+end
+
+# existsErr? returns true when the provided error is because the object already exists.
+def existsErr?(err)
+    err.end_with?("already exists")
+end
+
+# runWithData runs command specified in 'cmd' and writes 'data' to it's STDIN
+# It returns false if there is no error.
+def runWithData(cmd, data)
+    err = `printf "#{data}" | #{cmd}`
+    puts "error: #{err}"
+    if !$?.success?
+        return err
+    end
+    return false
+end
